661e894ed60b0ad4d95cbf7dc3acda7afe4cc1ce
uladzimir <01myit@gmail.com>
2025-05-26 19:56:56 +0300

    PointerEvent: pontermove. Handlers and capturing and bubbling.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
+<script lang="ts">
+	import { reactCount } from './counter.svelte'
+	const count = reactCount()
+	let clientX = $state(0)
+	let clientY = $state(0)
+	let pointerType = $state('')
+	function onpointermove(pointerEvent: any) {
+		clientX = pointerEvent.clientX
+		clientY = pointerEvent.clientY
+		pointerType = pointerEvent.pointerType
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h2>Count: {count.count}</h2>
+<button onclick={() => count.count++}>Increment count</button>
+<hr />
+
+<h2>Pointer type: {pointerType}</h2>
+<h2>Y: {clientY}</h2>
+<h2>X: {clientX}</h2>
+
+<div class="div" {onpointermove}></div>
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div class="capture" onkeydown={(e) => alert(`Div: ${e.key}`)}>
+	Bubbling <input
+		type="text"
+		placeholder="write a char"
+		onkeydown={(e) => alert(`Input: ${e.key}`)}
+	/>
+</div>
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div class="capture" onkeydowncapture={(e) => alert(`Div: ${e.key}`)}>
+	Capturing <input
+		type="text"
+		placeholder="write a char"
+		onkeydowncapture={(e) => alert(`Input: ${e.key}`)}
+	/>
+</div>
+
+<style>
+	.div {
+		width: 100vw;
+		height: 5vh;
+		background-color: grey;
+		padding: 2rem;
+	}
+	.capture,
+	input {
+		padding: 0.5rem;
+	}
+</style>
--- src/routes/counter.svelte.js
+++ src/routes/counter.svelte.js
@@ -1,4 +1,3 @@
-// place files you want to import through the `$lib` alias in this folder.
 export function reactCount() {
 	let count = $state(0)
 	return {
