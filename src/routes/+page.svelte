1c6661c77356083a25a1d8290bbbc1d6dac70c75
uladzimir <01myit@gmail.com>
2025-05-26 21:02:39 +0300

    bind:group and reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,20 @@
 <script lang="ts">
-	import { reactCount } from './counter.svelte'
-	const count = reactCount()
-	let clientX = $state(0)
-	let clientY = $state(0)
-	let pointerType = $state('')
-	function onpointermove(pointerEvent: any) {
-		clientX = pointerEvent.clientX
-		clientY = pointerEvent.clientY
-		pointerType = pointerEvent.pointerType
-	}
+	let colors: string[] = $state([])
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<h2>Count: {count.count}</h2>
-<button onclick={() => count.count++}>Increment count</button>
-<hr />
-
-<h2>Pointer type: {pointerType}</h2>
-<h2>Y: {clientY}</h2>
-<h2>X: {clientX}</h2>
-
-<div class="div" {onpointermove}></div>
-<!-- svelte-ignore a11y_no_static_element_interactions -->
-<div class="capture" onkeydown={(e) => alert(`Div: ${e.key}`)}>
-	Bubbling <input
-		type="text"
-		placeholder="write a char"
-		onkeydown={(e) => alert(`Input: ${e.key}`)}
-	/>
-</div>
-<!-- svelte-ignore a11y_no_static_element_interactions -->
-<div class="capture" onkeydowncapture={(e) => alert(`Div: ${e.key}`)}>
-	Capturing <input
-		type="text"
-		placeholder="write a char"
-		onkeydowncapture={(e) => alert(`Input: ${e.key}`)}
-	/>
-</div>
+<h1>Group contains next {colors.length === 1 ? 'color' : 'colors'}: {colors}</h1>
+{#each ['red', 'green', 'blue', 'brown'] as color, index (color + index)}
+	<label>
+		<input type="checkbox" value={color} bind:group={colors} />
+		{color}
+	</label>
+{/each}
 
 <style>
-	.div {
-		width: 100vw;
-		height: 5vh;
-		background-color: grey;
-		padding: 2rem;
+	label {
+		display: block;
 	}
-	.capture,
 	input {
-		padding: 0.5rem;
+		padding: 0.5rem 1rem 0.5rem 0.5rem;
 	}
 </style>
