3589c8e194ef856ba30a49673bf943a79fd6a0b7
uladzimir <01myit@gmail.com>
2025-06-02 20:45:41 +0300

    svelte:element with select and options.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,8 @@
 	// import UserClass from './UserClass.svelte'
 	// import Tween from './Tween.svelte'
 	// import Contenteditable from './Contenteditable.svelte'
-	import Todos from './Todos.svelte'
+	// import Todos from './Todos.svelte'
+	import SvelteElement from './SvelteElement.svelte'
 </script>
 
-<Todos />
+<SvelteElement />
---
+++ src/routes/SvelteElement.svelte
@@ -0,0 +1,12 @@
+<script>
+	let tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+	let value = $state()
+	$inspect(value)
+</script>
+
+<select bind:value>
+	{#each tags as tag, i}
+		<option value={tags[i]}>{tag.toLocaleUpperCase()}</option>
+	{/each}
+</select>
+<svelte:element this={value}>Svelte element</svelte:element>
