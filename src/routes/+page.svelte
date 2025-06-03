a37df0e936fd4a408ca4d97b558fac4876dc6abf
uladzimir <01myit@gmail.com>
2025-06-03 19:08:06 +0300

    svelte:boundary with onerror handler and snippet with failed and reset.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,19 @@
 <script>
+	// @ts-nocheck
+
 	// import UserClass from './UserClass.svelte'
 	// import Tween from './Tween.svelte'
 	// import Contenteditable from './Contenteditable.svelte'
 	// import Todos from './Todos.svelte'
-	import SvelteElement from './SvelteElement.svelte'
+	// import SvelteElement from './SvelteElement.svelte'
+	import SvelteBoundary from './SvelteBoundary.svelte'
 </script>
 
-<SvelteElement />
+<svelte:boundary onerror={(error) => console.log('Sending the error message')}>
+	<SvelteBoundary />
+	{#snippet failed(error, reset)}
+		<h1>OOOps!!!</h1>
+		<h3>{error.message}</h3>
+		<button onclick={reset}>RESET</button>
+	{/snippet}
+</svelte:boundary>
---
+++ src/routes/SvelteBoundary.svelte
@@ -0,0 +1,17 @@
+<script>
+	// @ts-nocheck
+
+	let mouse = $state({ x: 0, y: 0 })
+	// @ts-ignore
+	function onmousemove(e) {
+		mouse.x = e.x
+		mouse.y = e.y
+	}
+</script>
+
+<svelte:window {onmousemove} />
+
+<h2>Mouse X: {mouse.x}</h2>
+<h2>Mouse Y: {mouse.y}</h2>
+
+<button onclick={() => (mouse = null)}>Never touch me!</button>
